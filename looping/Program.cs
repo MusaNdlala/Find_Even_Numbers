52fa537de3db0960079111c601d1ad3cb11b7a71
Musa <Musa@DESKTOP-N08JB8A>
2021-09-10 17:25:02 +0200

    Add project files.

---
+++ looping/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace looping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int j = 1 % 1;
+            //Console.WriteLine(j);
+            evenNum(1,50);
+            Console.ReadLine();
+        }
+        static void evenNum(Int16 intStartNum, Int16 intEndNum){
+            while (intStartNum <= intEndNum)
+            {
+                if ((intStartNum % 2) == 0)
+                {
+                    Console.WriteLine(intStartNum);
+                }
+                intStartNum++;
+            }
+        }
+    }
+}
